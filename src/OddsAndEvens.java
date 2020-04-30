6a35aee732e365a7c30b5149b11ed091a97961de
austenhasty <hastyae@gmail.com>
2020-04-30 11:28:49 -0700

    Initial commit

---
+++ src/OddsAndEvens.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class OddsAndEvens {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Let's play a game called \" Odds and Evens \"");
+        System.out.print("What is your name? ");
+        String name = input.nextLine();
+        System.out.print("Hello " + name + ", which do you choose? (O)dds or (E)vens?");
+
+    }
+}
